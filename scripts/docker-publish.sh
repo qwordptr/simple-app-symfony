60557306ae68abff1ba67f674b23b90cbdcfed28
bielu000 <pbiel7@gmail.com>
2018-03-25 14:15:41 +0200

    Update CI

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo "Docker publish"
+DOCKER_ENV=''
+DOCKER_TAG=''
+
+case "$TRAVIS_BRANCH" in
+   "master")
+       DOCKER_ENV=production
+       DOCKER_TAG=latest
+     ;;
+    "dev")
+       DOCKER_ENV=dev
+       DOCKER_TAG=dev
+     ;;
+esac
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
+docker build -f ./Dockerfile -t simple-app-symfony:$DOCKER_TAG . --no-cache
+
+docker push $DOCKER_USERNAME/memotime.backend:$DOCKER_TAG
